79af9fae9ff7eb9d675f7525787ce6300f15b656
asqd <asqd117@gmail.com>
2014-07-01 19:28:14 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Buildingonmap::Application.config.secret_key_base = 'cd26ebdbab7a5aca56b1c3480ca846aa354c6ff8159de77915f3a9a321d415c11db5328dc8de458d49c7c5155c3d661cfb5e8e6971773b82e93327ebff8e9fc2'
