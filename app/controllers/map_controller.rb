05d709b5e7daf0aeb46f362f7729b42cc0a69bde
asqd <asqd117@gmail.com>
2014-07-06 01:57:07 +0400

    Refactor buildings controller

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -8,21 +8,30 @@ class MapController < ApplicationController
 
   def show
     point = Building.new(latitude: params[:latitude], longitude: params[:longitude])
-    
-    distanses = {}
-  	@buildings = point.nearbys(4)
 
-    @buildings.each do |building|
-		d = building.distance_to(point)
-		distanses[d] = building   
-	end
+    @buildings = point.nearbys(4)
     
+    distanses = compute_distanse(@buildings, point)
+
     @buildings = distanses.sort_by { |k, v| k }
 
     respond_to do |format|
       format.html { redirect_to root_url }
       format.js {}
     end
+  end
+
+  private
+
+  def compute_distanse(buildings, point)
+    distanses = {}
+
+    buildings.each do |building|
+      d = building.distance_to(point)
+      distanses[d] = building
+    end
+
+    distanses
   end
 
 end
